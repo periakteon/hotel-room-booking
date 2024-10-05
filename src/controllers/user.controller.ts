b39acbebd98578db6f15ce372d5addf247bd9d88
Masum Gökyüz <masumgokyuz@gmail.com>
2024-10-05 17:53:03 +0300

    refactor: logging

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import { CreateUserInput } from "../schemas/user.schema";
 import { createUser } from "../services/user.service";
 import { MongooseError } from "mongoose";
 import isMongoError from "../utils/mongoError";
+import log from "../utils/logger";
 
 export async function createUserHandler(
   req: Request<object, object, CreateUserInput>,
@@ -14,19 +15,23 @@ export async function createUserHandler(
     return res.send({ success: true, data: user.email });
   } catch (e: unknown) {
     if (isMongoError(e) && e.code === 11000) {
+      log.error(e, "Email is already in use");
       return res
         .status(400)
         .send({ success: false, message: "Email is already in use" });
     }
 
     if (e instanceof MongooseError) {
+      log.error(e, "Error occurred while creating user in mongoose");
       return res.status(400).send({ success: false, message: e.message });
     }
 
     if (e instanceof Error) {
+      log.error(e, "Error occurred while creating user");
       return res.status(400).send({ success: false, message: e.message });
     }
 
+    log.error(e, "Internal Server Error");
     return res
       .status(500)
       .send({ success: false, message: "Internal Server Error" });
--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { AnyZodObject, ZodError } from "zod";
 import { fromError } from "zod-validation-error";
+import log from "../utils/logger";
 
 function validateRequest(schema: AnyZodObject) {
   return function (req: Request, res: Response, next: NextFunction) {
@@ -13,13 +14,16 @@ function validateRequest(schema: AnyZodObject) {
       next();
     } catch (e: unknown) {
       if (e instanceof ZodError) {
+        log.error(e, "Error occurred while validating request");
         return res.status(400).send({ errors: fromError(e).message });
       }
 
       if (e instanceof Error) {
+        log.error(e, "Unknown error occurred");
         return res.status(400).send({ error: e.message });
       }
 
+      log.error(e, "Unknown error occurred");
       return res.status(400).send({ error: "Unknown error occurred" });
     }
   };
